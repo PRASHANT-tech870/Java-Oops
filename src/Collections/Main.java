18d4c71243c6c66592eec16f886a5df4c1619275
PRASHANT-tech870 <prashantronad870@gmail.com>
2025-05-08 15:30:28 +0530

    Cloning

---
+++ src/Collections/Main.java
@@ -0,0 +1,15 @@
+package Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+
+        -> see the image in asus tech 870
+        ->   arraylist is not synchronised - many threads can access at same time
+        -> vector is synchronised - many threads cant access at same time queuing happens
+
+         */
+
+
+    }
+}
